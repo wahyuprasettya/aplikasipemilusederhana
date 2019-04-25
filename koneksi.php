2dee2aa8fda5af6153a385dea0ff0802e38835a6
wahyu adjie prasetyo <32014966+wahyuprasettya@users.noreply.github.com>
2019-04-25 19:09:07 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,18 @@
+<?php 
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$db = "vote";
+
+$koneksi = mysqli_connect($server,$user,$password,$db);
+
+// if ($koneksi) {
+// 	echo "sukses";
+// }else{
+// 	echo "gagal";
+// }
+
+
+
+?>
